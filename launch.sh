b1d39816182f49ee5bb5b249e63ca13a4a980b62
sebgiles <sebgiles@icloud.com>
2019-10-22 01:43:46 +0200

    removed some things

--- launch.sh
+++ launch.sh
@@ -5,4 +5,3 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
-#roslaunch braitenberg_node braitenberg_node.launch veh:=$VEHICLE_NAME
