002fe6cfc932b074b236e58d7456d7de4903c492
sebgiles <sebgiles@icloud.com>
2019-10-30 22:16:22 +0100

    update with some useful features

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-
+git clone this repo in a folder named 'jupyter-node'
+cd into it
+If you want to connect to a ROS master running on another computer (such as a duckiebot) you just have to modify the hostnames and IP addresses in the run script 
--- launch.sh
+++ launch.sh
@@ -4,4 +4,5 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+cd /data/notebooks
+jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.password='password' --NotebookApp.token='password'
